0eaa744250a696dc887187b46aa63ac49a626ff7
CloroxBleach6 <39915632+CloroxBleach6@users.noreply.github.com>
2018-12-06 20:00:16 -0500

    Update clear.js

--- BotCommands/clear.js
+++ BotCommands/clear.js
@@ -5,19 +5,16 @@ module.exports.run = async (bot, message, args) => {
         message.reply("Usage: ;clear <number of messages>");
         return;
         }
-
-        await message.delete()
-
+    
         if(!message.guild.member(bot.user).hasPermission("MANAGE_MESSAGES")){
             return message.reply(":x: " + "| I need the \"MANAGE_MESSAGES\" permission!").catch(console.error);
           }
-
-
+    
+           await message.delete()
 
 if(!args[0]) return message.channel.send("please give me a number between 1 and 100");
 message.channel.bulkDelete(args[0]).then(() => {
     message.channel.send(`Cleared ${args[0]} messages`).then(msg => msg.delete(100));
-
 
 });
 
