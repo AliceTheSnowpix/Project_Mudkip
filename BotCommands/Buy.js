dd9a22fcb4634fd1e627251d01ffaa3aa39d4678
CloroxBleach6 <39915632+CloroxBleach6@users.noreply.github.com>
2018-12-10 11:05:17 -0500

    Create Buy.js

---
+++ BotCommands/Buy.js
@@ -0,0 +1,17 @@
+const db = require('quick.db')
+exports.run = async(bot, message, args) => {
+    let selfbubbles = await db.fetch(`bubbles_${message.author.id}`)
+    if(args.join("test"))
+        {
+        let cost = 1
+                db.subtract(`bubbles_${message.author.id}`, cost)
+                message.guild.member(message.author.id).addRole('521718720415006725')
+        }
+
+exports.config = {
+    allies: [ ]
+}
+
+exports.help = {
+    name: 'buy'
+ }
