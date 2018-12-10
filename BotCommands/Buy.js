cb6fddf71c162f4b2aed24d45d8c9eee187dfef5
CloroxBleach6 <39915632+CloroxBleach6@users.noreply.github.com>
2018-12-10 12:51:21 -0500

    Update Buy.js

--- BotCommands/Buy.js
+++ BotCommands/Buy.js
@@ -6,7 +6,7 @@ exports.run = async(bot, message, args) => {
         let cost = 1
                 db.subtract(`bubbles_${message.author.id}`, cost)
                 message.guild.member(message.author.id).addRole('521718720415006725')
-                message.reply('I have added the ${args.join("test")} role`)
+                message.reply(`I have added the ${args.join("test")} role`)
         }
     }
 
