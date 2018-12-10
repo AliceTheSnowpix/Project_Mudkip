d7d3e1905a34204a305a722c3fc16aed2ae009ce
CloroxBleach6 <39915632+CloroxBleach6@users.noreply.github.com>
2018-12-10 10:31:54 -0500

    Update songhelp.js

--- BotCommands/songhelp.js
+++ BotCommands/songhelp.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
 let shembed = new Discord.RichEmbed()
     .setDescription("Song Help Menu")
     .setColor("#FF00EC")
-    .addField("Song Commands", "alien, alive, bad, bellyache, bored, break, broken, broken2, burn, copycat, crown, damn, dirty, ending, enemies, entropy, fakelove, fireflies, friends, friendship, god, happysong, hostage, idwbya, japanese, lovely, lush, middle, motto, my, myboy, nights, ocean, off, og, panic, pumpedup, ripsong, rocksong, sadsong, side, six, ski, snow, something, starting, summertime, sun, sunshine, surf, team, tennis, trials, and why")
+    .addField("Song Commands", "alien, alive, bad, bellyache, bored, break, broken, broken2, burn, copycat, crown, damn, dirty, ending, enemies, entropy, fakelove, fireflies, friends, friendship, god, happysong, hostage, idwbya, japanese, lovely, lush, middle, motto, my, myboy, nights, ocean, off, og, panic, pumpedup, ripsong, rocksong, sadsong, side, six, ski, smoothie, snow, something, starting, story, summertime, sun, sunshine, surf, team, tennis, trials, warpstar, and why")
     
     message.channel.send(shembed)
 } 
