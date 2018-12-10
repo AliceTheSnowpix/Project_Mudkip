3a1a42d5f7d9ea1f0b103559b3dac5ed958b91c3
CloroxBleach6 <39915632+CloroxBleach6@users.noreply.github.com>
2018-12-10 13:07:12 -0500

    Update Buy.js

--- BotCommands/Buy.js
+++ BotCommands/Buy.js
@@ -1,9 +1,12 @@
 const db = require('quick.db')
 exports.run = async(bot, message, args) => {
     let selfbubbles = await db.fetch(`bubbles_${message.author.id}`)
+ 
     if(args.join("test"))
         {
         let cost = 1
+        amount = parseInt(cost)
+            if (amount > selfbubbles) return message.reply("**Sorry you do not have enough bubbles to buy that item**")
                 db.subtract(`bubbles_${message.author.id}`, cost)
                 message.guild.member(message.author.id).addRole('521718720415006725')
                 message.reply(`I have gave you the ${args} role and removed ${cost} bubbles)
