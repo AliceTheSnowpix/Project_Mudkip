48339e44f682b9ec416443ef8c6c16ae4d033b8f
CloroxBleach6 <39915632+CloroxBleach6@users.noreply.github.com>
2018-12-13 08:24:53 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,25 +26,6 @@ fs.readdir("./BotCommands/", (err, files) => {
 });
 });
 
-fs.readdir("./BotCommands/songcommands/", (err, files) => {
-  if(err) console.log(err);
-
-  let jsfile = files.filter(f => f.split(".").pop() === "js")
-  if(jsfile.length <= 0){
-      console.log("Could not find commands");
-      return;
-  }
-
-  jsfile.forEach((f, i) =>{
-      let props = require(`./BotCommands/songcommands/${f}`);
-      console.log(`${f} Files loaded`);
-      bot.commands.set(props.help.name, props)
-      props.config.aliases.forEach(alias => {
-      bot.aliases.set(alias, props.help.name);
-  });
-});
-});
-
 fs.readdir("./BotCommands/musiccommands/", (err, files) => {
   if(err) console.log(err);
 
