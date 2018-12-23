0817cb2282c548720596cac4adfec51c26ff2eb5
CloroxBleach6 <39915632+CloroxBleach6@users.noreply.github.com>
2018-12-23 03:07:34 -0500

    Update cloroxmix.js

--- BotCommands/songcommands/cloroxmix.js
+++ BotCommands/songcommands/cloroxmix.js
@@ -18,11 +18,10 @@ exports.run = async (bot,message,args) => {
         const dispatcher = connection.playStream('https://cdn.glitch.com/3266dedb-0ed2-42c3-a45d-8cb0a8559df0%2Fbreak.mp3?1544921325229');
         if (dispatcher.on("speaking", speaking => speaking == true)){
         } 
-        if (dispatcher.on('end', end => dispatcher = connection.playStream('https://cdn.glitch.com/3266dedb-0ed2-42c3-a45d-8cb0a8559df0%2Fentropy.mp3?1544921239890'))) {
-        dispatcher.on('end', end => {
-        dispatcher = connection.playStream('https://cdn.glitch.com/3266dedb-0ed2-42c3-a45d-8cb0a8559df0%2Ffireflies.mp3?1544921586964')
-          })
+        if (dispatcher.on('end', end => connection.playStream('https://cdn.glitch.com/3266dedb-0ed2-42c3-a45d-8cb0a8559df0%2Fentropy.mp3?1544921239890'))) {
+        }else if (dispatcher.on('end', end => connection.playStream('https://cdn.glitch.com/3266dedb-0ed2-42c3-a45d-8cb0a8559df0%2Ffireflies.mp3?1544921586964'))){
         }
+        
         }).catch(err => console.log(err));
     }
     
