c8cd2a17313fa6bc6c9863ad3633a559047cdf29
CloroxBleach6 <39915632+CloroxBleach6@users.noreply.github.com>
2018-12-23 13:58:46 -0500

    Update cloroxmix.js

--- BotCommands/songcommands/cloroxmix.js
+++ BotCommands/songcommands/cloroxmix.js
@@ -19,11 +19,10 @@ exports.run = async (bot,message,args) => {
         if (dispatcher.on("speaking", speaking => speaking == true)){
         } 
         if (dispatcher.on('end', end => connection.playStream('https://cdn.glitch.com/3266dedb-0ed2-42c3-a45d-8cb0a8559df0%2Fentropy.mp3?1544921239890'))) {
-        }else if (dispatcher.on('end', end => connection.playStream('https://cdn.glitch.com/3266dedb-0ed2-42c3-a45d-8cb0a8559df0%2Ffireflies.mp3?1544921586964'))){
+            connection.playStream('https://cdn.glitch.com/3266dedb-0ed2-42c3-a45d-8cb0a8559df0%2Ffireflies.mp3?1544921586964')
         }
-        
-        }).catch(err => console.log(err));
-    }
+    }).catch(err => console.log(err));
+}
     
 exports.config = {
     aliases: [  ]
