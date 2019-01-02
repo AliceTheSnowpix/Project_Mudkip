59befc232e7b3c8b1e4bbdd1093a24c4e56e56e8
CloroxBleach6 <39915632+CloroxBleach6@users.noreply.github.com>
2019-01-02 15:55:40 -0500

    Update message-user.js

--- BotCommands/message-user.js
+++ BotCommands/message-user.js
@@ -2,9 +2,8 @@ const ownerID = '293148538886553602'
 
 exports.run = async (bot,message,args) => {
     if (message.author.id != ownerID) return message.channel.send("Bot Owner Only");
-    let guild = args[0]
-    let user = args[1]
-    bot.guilds.get(guild).members.get(user).send(args.slice(2).join(' '))
+    let user = args[0]
+    bot.users.get(user).send(args.slice(1).join(' '))
 }
 
 exports.config = {
