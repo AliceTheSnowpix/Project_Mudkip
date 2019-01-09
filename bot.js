1b79e742e0da2a50b24249075575309155a3f6eb
CloroxBleach6 <39915632+CloroxBleach6@users.noreply.github.com>
2019-01-09 08:29:37 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,9 +29,63 @@ fs.readdir("./BotCommands/", (err, files) => {
 });
 });
 
-let loadfiles = require("./Modules/loadfiles.js")
-loadfiles.run();
+fs.readdir("./BotCommands/songcommands/", (err, files) => {
+  if(err) console.log(err);
 
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+      console.log("Could not find commands");
+      return;
+  }
+
+  jsfile.forEach((f, i) =>{
+      let props = require(`./BotCommands/songcommands/${f}`);
+      console.log(`${f} Files loaded`);
+      bot.commands.set(props.help.name, props)
+      props.config.aliases.forEach(alias => {
+      bot.aliases.set(alias, props.help.name);
+  });
+});
+});
+
+fs.readdir("./BotCommands/musiccommands/", (err, files) => {
+  if(err) console.log(err);
+
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+      console.log("Could not find commands");
+      return;
+  }
+
+  jsfile.forEach((f, i) =>{
+      let props = require(`./BotCommands/musiccommands/${f}`);
+      console.log(`${f} Files loaded`);
+      bot.commands.set(props.help.name, props)
+      props.config.aliases.forEach(alias => {
+      bot.aliases.set(alias, props.help.name);
+  });
+});
+});
+
+fs.readdir("./BotCommands/imagecommands/", (err, files) => {
+  if(err) console.log(err);
+
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+      console.log("Could not find commands");
+      return;
+  } 
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./BotCommands/imagecommands/${f}`);
+    console.log(`${f} Files loaded`);
+    bot.commands.set(props.help.name, props)
+    props.config.aliases.forEach(alias => {
+    bot.aliases.set(alias, props.help.name);
+});
+});
+});
+                        
 bot.on('ready', () => {
   const activities_list = [
     ";help commands",
