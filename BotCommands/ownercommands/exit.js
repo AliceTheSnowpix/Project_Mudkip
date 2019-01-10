b41b2f09c4115647d000fb7de6eed7c40517d275
CloroxBleach6 <39915632+CloroxBleach6@users.noreply.github.com>
2019-01-10 07:08:06 -0500

    Update exit.js

--- BotCommands/ownercommands/exit.js
+++ BotCommands/ownercommands/exit.js
@@ -1,9 +1,8 @@
-const botconfig = require('./botconfig/botconfig.json');
 const ownerID = '293148538886553602'
 exports.run = async (bot, message) => {
     await message.delete().catch();
     if (message.author.id != ownerID) return message.channel.send("Bot Owner Only");
-    await message.channel.send(`Restarting will be back soon, ${botconfig.ownerID}!`);
+    await message.channel.send("Restarting will be back soon, <@293148538886553602>!');
     await process.exit(0).catch((e) => { console.error(e); });
 };
   
