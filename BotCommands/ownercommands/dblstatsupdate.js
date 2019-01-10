fa88c652e493abb62a1a0366e4e96584bbc5ad69
CloroxBleach6 <39915632+CloroxBleach6@users.noreply.github.com>
2019-01-10 12:15:14 -0500

    Create dblstatsupdate.js

---
+++ BotCommands/ownercommands/dblstatsupdate.js
@@ -0,0 +1,27 @@
+axios = require("axios")
+require("dotenv")
+const ownerID = '293148538886553602'
+
+exports.run = async (bot, message, args) => {
+    if (message.author.id != ownerID) return message.channel.send("Bot Owner Only");
+    message.channel.send("Updating Stats On Dbl")
+
+axios.post(`https://discordbotlist.com/api/bots/${process.env.BOT_ID}/stats`, {
+    shard_id: 0,
+    guilds: bot.guilds.size,
+    users: bot.users.size,
+    voice_connections: bot.voiceConnections.size,
+}, {
+    headers: {
+        Authorization: `Bot ${process.env.DBLTOKEN}`,
+    }
+}).catch(console.error);
+}
+
+exports.config = {
+    aliases: [  ]
+};
+ 
+module.exports.help = {
+  name: "updatestats"
+}
