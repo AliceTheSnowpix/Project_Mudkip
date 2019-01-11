4b2516c1180f5fd46a2ec28f307c5702b853f042
CloroxBleach6 <39915632+CloroxBleach6@users.noreply.github.com>
2019-01-11 12:13:11 -0500

    Update search.js

--- BotCommands/musiccommands/search.js
+++ BotCommands/musiccommands/search.js
@@ -12,11 +12,11 @@ exports.run = async(bot, message, args, ops) => {
             resp += `**[${parseInt(i)+1}]:** \`${videos[i].title}\`\n`;
         }
 
-        resp += `\n**Choose a number between** \` 1-${videos.length}\``;
+        resp += `\n**Choose a number between** \` 1-${videos.length - 1}\``;
 
         message.channel.send(resp);
 
-        const filter = m => !isNaN(m.content) && m.content < videos.length && m.content > 0;
+        const filter = m => !isNaN(m.content) && m.content < videos.length + 1 && m.content > 0;
         const collector = message.channel.createMessageCollector(filter);
 
         collector.videos = videos;
