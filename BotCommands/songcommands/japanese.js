a86ce42714e9220d0fe6fc5d01981548fca84795
CloroxBleach6 <39915632+CloroxBleach6@users.noreply.github.com>
2019-02-02 13:49:10 -0500

    Update japanese.js

--- BotCommands/songcommands/japanese.js
+++ BotCommands/songcommands/japanese.js
@@ -14,11 +14,9 @@ module.exports.run = async (bot,message,args) => {
 voiceChannel.join().then(connection =>
 {
     const dispatcher = connection.playStream('https://cdn.glitch.com/3266dedb-0ed2-42c3-a45d-8cb0a8559df0%2Fjapanese.mp3?1544921703881');
-    if (dispatcher.on("speaking", speaking => speaking == true)){
-        dispatcher
-    } 
-    if (dispatcher.on('end', end => end == voiceChannel.leave())){ 
-    }
+    dispatcher.on("end", end => {
+        voiceChannel.leave();
+    })
     }).catch(err => console.log(err));
 }
 
