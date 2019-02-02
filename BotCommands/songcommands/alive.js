a1b5a2a3efa807c1a95ff212812629c714416cbe
CloroxBleach6 <39915632+CloroxBleach6@users.noreply.github.com>
2019-02-02 11:25:25 -0500

    Update alive.js

--- BotCommands/songcommands/alive.js
+++ BotCommands/songcommands/alive.js
@@ -16,10 +16,9 @@ module.exports.run = async (bot,message,args) => {
 voiceChannel.join().then(connection =>
 {
     const dispatcher = connection.playStream('https://cdn.glitch.com/3266dedb-0ed2-42c3-a45d-8cb0a8559df0%2Falive.mp3?1544923039755');
-    if (dispatcher.on("speaking", speaking => speaking == true)){
-    } 
-    if (dispatcher.on('end', end => end == voiceChannel.leave())){ 
-    }
+    dispatcher.on('end', end => {
+        voiceChannel.leave();
+    })
     }).catch(err => console.log(err));
 }
 
