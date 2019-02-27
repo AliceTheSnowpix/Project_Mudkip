8d3dc0b26cd9f504eb43f50caeac28afa64f3485
CloroxBleach6 <39915632+CloroxBleach6@users.noreply.github.com>
2019-02-27 16:32:43 -0500

    Update wouldyourather.js

--- BotCommands/funcommands/wouldyourather.js
+++ BotCommands/funcommands/wouldyourather.js
@@ -7,9 +7,9 @@ module.exports.run = async (bot, message, args) => {
     .setColor("#f99d9d")
     .setDescription(question[Math.floor(Math.random() * question.length)])
 
-  message.channel.send(wouldembed).then(msg => { 
-    msg.react("1⃣");
-    msg.react("2⃣");
+  message.channel.send(wouldembed).then(async msg => { 
+    await msg.react("1⃣");
+    await msg.react("2⃣");
   });
 };
 
