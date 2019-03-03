5554abfeaa5534e7108e6e13ffaa8d2bb057cdf0
CloroxBleach6 <39915632+CloroxBleach6@users.noreply.github.com>
2019-03-03 15:04:41 -0500

    Update unban.js

--- BotCommands/modcommands/unban.js
+++ BotCommands/modcommands/unban.js
@@ -1,12 +1,22 @@
+const discord = require("discord.js");
+
 exports.run = (bot, message, args) => {
   const reason = args.slice(1).join(' ');
-  bot.unbanReason = reason;
-  bot.unbanAuth = message.author;
   const user = args[0];
   const modlog = bot.channels.find('a => a.name === modlogs');
   if (!modlog) return message.reply('I cannot find a modlogs channel');
   if (reason.length < 1) return message.reply('You must supply a reason for the unban.');
   if (!user) return message.reply('You need to send the id of the user you want to unban').catch(console.error);
+  let unbanEmbed = new discord.RichEmbed()
+  .setColor('00AAFF')
+  .setTitle("Unban")
+  .addField("Unbanned User", `<@${user}> with ID ${user}`)
+  .addField("Unbanned By", `<@${message.author.id}> with ID ${message.author.id}`)
+  .addField("Unbanned In", message.channel)
+  .addField("Time", message.createdAt)
+  .addField("Reason", reason);
+  
+  modlog.send(unbanEmbed);
   message.guild.unban(user);
 };
 
