d56a34acafa66c4bfc90828acf8e8204741c546f
CloroxBleach6 <39915632+CloroxBleach6@users.noreply.github.com>
2019-03-08 17:37:57 -0500

    Update meme.js

--- BotCommands/imagecommands/meme.js
+++ BotCommands/imagecommands/meme.js
@@ -1,19 +1,15 @@
 const Discord = require("discord.js");
 const randomPuppy = require("random-puppy");
 
-module.exports.run = async (bot, message, args) => {
-  if(message.author.bot) return;
-  if(message.channel.type !=="text") return;
-  
-   randomPuppy('memes')
-  .then(url => {
-                const embed = new Discord.RichEmbed()
-                    .setTitle('Here is a meme')
-                    .setTimestamp()
-                    .setImage(url)
-                    .setColor(`RANDOM`)
-                     message.channel.send({ embed });
-  })
+exports.run = async (bot, message, args) => {
+   randomPuppy.all('dankmemes').then(url => {
+      const embed = new Discord.RichEmbed()
+          .setTitle('Here is a meme')
+          .setTimestamp()
+          .setImage(url)
+          .setColor(`RANDOM`)
+      message.channel.send(embed);
+  });
 }
 
 exports.config = {
