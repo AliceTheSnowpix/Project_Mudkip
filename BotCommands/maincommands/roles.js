d9cf75a3040779de6c4839d4ecda15e0e4205e6f
CloroxBleach6 <39915632+CloroxBleach6@users.noreply.github.com>
2019-03-18 15:53:28 -0400

    Update roles.js

--- BotCommands/maincommands/roles.js
+++ BotCommands/maincommands/roles.js
@@ -13,7 +13,23 @@ module.exports.run = async (bot, message, args) => {
     .setDescription(roles.slice(i * 1, (i * 25) + 25).join('\n'))
     .setFooter(`Page: ${i + 1} of ${noOfPages > parseInt(noOfPages) ? parseInt(noOfPages) + 1 : parseInt(noOfPages)}`)
 
-    message.channel.send(rolesembed)
+    message.channel.send(rolesembed).then(msg => { 
+    
+            msg.react('⏪').then( r => { 
+                msg.react('⏩') 
+            
+                const backwardsFilter = (reaction, user) => reaction.emoji.name === '⏪' && user.id === message.author.id;
+                const forwardsFilter = (reaction, user) => reaction.emoji.name === '⏩' && user.id === message.author.id; 
+                
+                const backwards = msg.createReactionCollector(backwardsFilter, { time: 60000 }); 
+                const forwards = msg.createReactionCollector(forwardsFilter, { time: 60000 }); 
+       forwards.on('collect', r => { 
+           let rolesembed = new Discord.RichEmbed()
+            .setColor("#84ff96")
+            .setTitle("Roles")
+            .setDescription(roles.slice(i * 1, (i * 50) + 50).join('\n'))
+            .setFooter(`Page: ${i + 1} of ${noOfPages > parseInt(noOfPages) ? parseInt(noOfPages) + 1 : parseInt(noOfPages)}`)
+       }
 
 }
 
