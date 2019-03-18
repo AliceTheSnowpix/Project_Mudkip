fcefb630816f8cc4be1ea34678441fb31ea983ed
CloroxBleach6 <39915632+CloroxBleach6@users.noreply.github.com>
2019-03-18 16:16:50 -0400

    Update roles.js

--- BotCommands/maincommands/roles.js
+++ BotCommands/maincommands/roles.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 
-exports.run = async (bot, message, args) => {
+module.exports.run = async (bot, message, args) => {
     let roles = message.guild.roles.filter(r => r.position !== 0).map(r => `${r.name} - ${r.id}`).map((r, i) => `${i + 1}. ${r}`);
   
     let noOfPages = roles.length / 25;
@@ -13,32 +13,14 @@ exports.run = async (bot, message, args) => {
     .setDescription(roles.slice(i * 1, (i * 25) + 25).join('\n'))
     .setFooter(`Page: ${i + 1} of ${noOfPages > parseInt(noOfPages) ? parseInt(noOfPages) + 1 : parseInt(noOfPages)}`)
 
-    message.channel.send(rolesembed).then(msg => { 
-    
-    msg.react('⏪').then( r => { 
-        msg.react('⏩') 
+    message.channel.send(rolesembed)
 
-        const backwardsFilter = (reaction, user) => reaction.emoji.name === '⏪' && user.id === message.author.id;
-        const forwardsFilter = (reaction, user) => reaction.emoji.name === '⏩' && user.id === message.author.id; 
-
-        const backwards = msg.createReactionCollector(backwardsFilter, { time: 60000 }); 
-        const forwards = msg.createReactionCollector(forwardsFilter, { time: 60000 }); 
-        forwards.on('collect', r => { 
-            let rolesembed = new Discord.RichEmbed()
-            .setColor("#84ff96")
-            .setTitle("Roles")
-            .setDescription(roles.slice(i * 1, (i * 50) + 50).join('\n'))
-            .setFooter(`Page: ${i + 1} of ${noOfPages > parseInt(noOfPages) ? parseInt(noOfPages) + 1 : parseInt(noOfPages)}`)
-            msg.edit(rolesembed)
-           });
-        });
-    });
 }
 
-exports.config = {
+module.exports.config = {
     aliases: []
   };
 
-exports.help = {
+module.exports.help = {
     name: "roles",
 }
