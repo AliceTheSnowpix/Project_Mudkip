c9ca475dfcc3d802adc327bc4120225d02515b62
CloroxBleach6 <39915632+CloroxBleach6@users.noreply.github.com>
2019-03-28 18:57:53 -0400

    Update server.js

--- BotCommands/maincommands/server.js
+++ BotCommands/maincommands/server.js
@@ -1,47 +1,49 @@
 const Discord = require('discord.js');
 
-module.exports.run = async (bot,message,args) => {
+exports.run = async (bot, message, args) => {
 
     let serverembed = new Discord.RichEmbed()
 
     let verifLevels = ["None", "Low", "Medium", "(╯°□°）╯︵  ┻━┻", "┻━┻ミヽ(ಠ益ಠ)ノ彡┻━┻"];
-        let region = {
-            "brazil": "Brazil",
-            "eu-central": "Central Europe",
-            "singapore": "Singapore",
-            "us-central": "U.S. Central",
-            "sydney": "Sydney",
-            "us-east": "U.S. East",
-            "us-south": "U.S. South",
-            "us-west": "U.S. West",
-            "eu-west": "Western Europe",
-            "vip-us-east": "VIP U.S. East",
-            "london": "London",
-            "amsterdam": "Amsterdam",
-            "hongkong": "Hong Kong"
-        };
-          
-        serverembed.setDescription("this is the server info")
-        .setColor("#27FF00")
-        .setThumbnail(message.guild.iconURL ? message.guild.iconURL : me.user.displayAvatarURL)
-        .addField("Server Name", message.guild.name)
-        .addField("Created By", message.guild.owner)
-        .addField("Joined At", message.guild.joinedAt)
-        .addField("Total Members", message.guild.memberCount)
-        .addField("Created On", message.guild.createdAt)
-        .addField("ID:", message.guild.id)
-        .addField("Region", region[message.guild.region], true)
-        .addField("Roles", message.guild.roles.size, true)
-        .addField("Channels", message.guild.channels.size, true)
-        .addField("Verification Level", verifLevels[message.guild.verificationLevel], true)    
+    let region = {
+        "brazil": "Brazil",
+        "eu-central": "Central Europe",
+        "singapore": "Singapore",
+        "us-central": "U.S. Central",
+        "sydney": "Sydney",
+        "us-east": "U.S. East",
+        "us-south": "U.S. South",
+        "us-west": "U.S. West",
+        "eu-west": "Western Europe",
+        "vip-us-east": "VIP U.S. East",
+        "london": "London",
+        "amsterdam": "Amsterdam",
+        "hongkong": "Hong Kong"
+    };
 
-         message.channel.send(serverembed);
+    serverembed.setDescription("this is the server info")
+    .setColor("#27FF00")
+    .setThumbnail(message.guild.iconURL ? message.guild.iconURL : me.user.displayAvatarURL)
+    .addField("Server Name", message.guild.name)
+    .addField("Created By", message.guild.owner)
+    .addField("Joined At", message.member.guild.joinedAt)
+    .addField("Member Count", message.guild.members.filter(u => !u.user.bot).size)
+    .addField("Bot Count", message.guild.members.filter(u => u.user.bot).size)
+    .addField("Total Members", message.guild.memberCount)
+    .addField("Created On", message.guild.createdAt)
+    .addField("ID:", message.guild.id)
+    .addField("Region", region[message.guild.region], true)
+    .addField("Roles", message.guild.roles.size, true)
+    .addField("Channels", message.guild.channels.size, true)
+    .addField("Verification Level", verifLevels[message.guild.verificationLevel], true)    
+
+    message.channel.send(serverembed);
 }
 
 exports.config = {
     aliases: [ 'serverinfo', 'server-info' ]
 };
 
-module.exports.help = {
+exports.help = {
     name: "server"
 }
