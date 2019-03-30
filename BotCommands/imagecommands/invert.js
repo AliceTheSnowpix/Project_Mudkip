6a9224440c121faf31f44acc6780137258c9ce08
CloroxBleach6 <39915632+CloroxBleach6@users.noreply.github.com>
2019-03-30 12:30:14 -0400

    Update invert.js

--- BotCommands/imagecommands/invert.js
+++ BotCommands/imagecommands/invert.js
@@ -3,19 +3,15 @@ const request = require('node-superfetch');
 const { invert } = require('../../util/Canvas');
 
 exports.run = async (bot, message, args) => {
-  try {
-      const { body } = await request.get(image);
-      const data = await loadImage(body);
-      const canvas = createCanvas(data.width, data.height);
-      const ctx = canvas.getContext('2d');
-      ctx.drawImage(data, 0, 0);
-      invert(ctx, 0, 0, data.width, data.height);
-      const attachment = canvas.toBuffer();
-      if (Buffer.byteLength(attachment) > 8e+6) return message.reply('Resulting image was above 8 MB.');
-      return message.channel.send({ files: [{ attachment, name: 'invert.png' }] });
-    } catch (err) {
-      return message.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
-  }
+    const { body } = await request.get(image);
+    const data = await loadImage(body);
+    const canvas = createCanvas(data.width, data.height);
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(data, 0, 0);
+    invert(ctx, 0, 0, data.width, data.height);
+    const attachment = canvas.toBuffer();
+    if (Buffer.byteLength(attachment) > 8e+6) return message.reply('Resulting image was above 8 MB.');
+    return message.channel.send({ files: [{ attachment, name: 'invert.png' }] });
 }
 
 exports.config = {
