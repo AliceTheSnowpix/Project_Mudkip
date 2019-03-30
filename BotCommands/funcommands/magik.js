9a1262f6dd6bc3b1fef24d80269635ea20343d19
CloroxBleach6 <39915632+CloroxBleach6@users.noreply.github.com>
2019-03-30 12:41:57 -0400

    Update magik.js

--- BotCommands/funcommands/magik.js
+++ BotCommands/funcommands/magik.js
@@ -1,19 +1,37 @@
-const Discord = require('discord.js');
-const snekfetch = require('snekfetch');
+const request = require("request");
+const tempy = require("tempy");
+const gm = require("@tohru/gm").subClass({
+  imageMagick: true
+});
 
-exports.run = async(bot, message, args) => {
-    let msg = await message.channel.send(':loading: Generating avatar...');
-    let mentionedUser = message.mentions.users.first() || message.author;
-    let avatar = 'https://discord.services/api/magik?url=' + mentionedUser.displayAvatarURL;
-    let avatarEmbed = new Discord.RichEmbed()
-        .setImage(avatar)
-        .setColor('#a0e5ff')
-        .setTitle('Magik')
-        .setDescription('[Magik Link](' + avatar + ')');
-
-    message.channel.send(avatarEmbed);
-    msg.delete();
-}
+exports.run = async (bot, message, args) => {
+  const image = await bot.getImage(message).catch(error => {
+    message.reply("you need to provide an image to add some magik!");
+    console.log(error);
+  });
+  const magikResize = tempy.file({ extension: "png" });
+  const magikTemp = tempy.file({ extension: "png" });
+  const magikOutput = tempy.file({ extension: "png" });
+  if (image !== undefined) {
+    const processMessage = await message.channel.send('Processing... This might take a while');
+    gm(request(image)).resize(800, 800).strip().write(magikResize, (error) => {
+      if (error) throw new Error(error);
+      gm(magikResize).out("-liquid-rescale", "400x400").strip().write(magikTemp, (error) => {
+        if (error) throw new Error(error);
+        gm(magikTemp).out("-liquid-rescale", "1200x1200").strip().write(magikOutput, async (error) => {
+          if (error) throw new Error(error);
+          await message.channel.send({
+            files: [{
+              attachment: magikOutput,
+              name: "magik.png"
+            }]
+          });
+          processMessage.delete();
+        });
+      });
+    });
+  }
+};
 
 exports.config = {
     aliases: [  ]
