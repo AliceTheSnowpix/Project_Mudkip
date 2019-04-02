61d282281d5cd60a6a123f683064fe1b182e1335
CloroxBleach6 <39915632+CloroxBleach6@users.noreply.github.com>
2019-04-02 15:32:36 -0400

    Update mute.js

--- BotCommands/modcommands/mute.js
+++ BotCommands/modcommands/mute.js
@@ -1,39 +1,50 @@
 const Discord = require('discord.js')
-exports.run = async (bot, message, args) => {
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Sorry you cant do that try again when mod");
-    if(args[0] == "help"){
-        message.reply("Usage: ;mute <user>");
-        return;
-    }
+exports.run = async (bot, message, args) => 
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Sorry you cant use this command because you do not have the manage messages permission");
   
-    let Mute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!Mute) return message.reply("You need to give me someone to mute or the person you said is incorrect");
+    let Mute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]))
+    
+    let errorEmbed = new Discord.RichEmbed()
+    .setTitle('Incorect Command Usage')
+    .setColor('MAGENTA')
+    .addField('Command usage', ';mute <member name / mention>')
+    .addField('Server mute usage', ';mute server <member name / mention>')
+    
+    if(!Mute) return message.reply(errorEmbed)
     let mreason = args.join(" ").slice(22);
     if(Mute.hasPermission("MANAGE_MESSAGES")) return message.reply("Can't mute them!");
-    let muterole = message.guild.roles.find(a => a.name === "muted");
-    //start of create role
-    if(!muterole){
-      try{
-        muterole = await message.guild.createRole({
-          name: "muted",
-          color: "#000000",
-          permissions:[]
-        })
-        message.guild.channels.forEach(async (channel, id) => {
-          await channel.overwritePermissions(muterole, {
-            SEND_MESSAGES: false,
-            ADD_REACTIONS: false
-          });
+
+    let arg = message.content.toLowerCase()    
+
+    if(arg === `;mute server ${Mute}`) {
+        message.guild.channels.forEach(channel=> {
+            channel.replacePermissionOverwrites(
+                { 
+                    overwrites: [ 
+                        {
+                            id: `${Mute.id}`, 
+                            denied: ['SEND_MESSAGES']
+                        }
+                    ]
+                }
+            });
         });
-      }catch(e){
-        console.log(e.stack);
-      }
+    } else {
+        message.channel.replacePermissionOverwrites(
+            { 
+                overwrites: [ 
+                    {
+                        id: `${Mute.id}`, 
+                        denied: ['SEND_MESSAGES']
+                    }
+                ]
+            }
+        });
     }
-    if(Mute.roles.has(muterole.id)) return message.reply("That person is already muted")
-    await(Mute.addRole(muterole.id));
+    
     let mutechan = message.guild.channels.find(b => b.name === "modlogs");
     if(!mutechan) return message.channel.send("Could not find modlogs channel");
-  if(!mreason){
+    if(!mreason){
 
     let muteembed2 = new Discord.RichEmbed()
     .setAuthor(message.author.username)
@@ -41,7 +52,7 @@ exports.run = async (bot, message, args) => {
     .setThumbnail(Mute.user.avatarURL)
     .setTimestamp(message.createdAt)
     .setDescription(`<@${Mute.id}> has been muted`)
-    .addField("There was no reason given", "So maybe next time a reason will be given")
+    .addField("There was no reason given", "test")
 
     message.delete().catch(O_o=>{});
     mutechan.send(muteembed2);
@@ -67,4 +78,4 @@ exports.config = {
 
 exports.help = {
     name: 'mute'
-}
+};
