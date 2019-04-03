04ffd2635f0475e759af4e9848f79ebcfc380e10
CloroxBleach6 <39915632+CloroxBleach6@users.noreply.github.com>
2019-04-03 16:29:58 -0400

    Update mute.js

--- BotCommands/modcommands/mute.js
+++ BotCommands/modcommands/mute.js
@@ -1,23 +1,23 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
 
 exports.run = async(bot, message, args) => {
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Sorry you cant use this command because you do not have the manage messages permission");
   
-    let Mute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]))
+    let Mute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     
     let errorEmbed = new Discord.RichEmbed()
     .setTitle('Incorect Command Usage')
     .setColor('PURPLE')
-    .addField('Command usage', ';mute <member name / mention>')
-    .addField('Server mute usage', ';mute server <member name / mention>')
+    .addField('Command usage', ';mute <member name / mention> <reason optional>')
+    .addField('Server mute usage', ';mute server <member name / mention> <reason optional>')
     
-    if(!Mute) return message.reply(errorEmbed)
-    let arg = message.content.toLowerCase() 
-    let mreason = args.join(" ").slice(22)
+    if(!Mute) return message.reply(errorEmbed);
+    let arg = message.content.toLowerCase(); 
+    let mreason = args.join(" ").slice(22);
+    let mreasonS = args.join(" ").slice(32);
     if(Mute.hasPermission("MANAGE_MESSAGES")) return message.reply("Can't mute them!");   
 
     if(arg === `;mute server ${Mute}`) {
-        let mreasonS = args.join(" ").slice(28)
         message.guild.channels.forEach(channel => {
             channel.overwritePermissions(message.guild.members.get(Mute.id),{
                 SEND_MESSAGES: false
@@ -35,7 +35,7 @@ exports.run = async(bot, message, args) => {
         .setThumbnail(Mute.user.avatarURL)
         .setTimestamp(message.createdAt)
         .setDescription(`<@${Mute.id}> has been muted in all channels`)
-        .addField("There was no reason given", `so I dont know why <@${Mute.id}> was muted`)
+        .addField("There was no reason given", `so I don't know why <@${Mute.id}> was muted`)
 
         message.delete().catch(O_o=>{});
         mutechanS.send(muteembed2S);
@@ -71,7 +71,7 @@ exports.run = async(bot, message, args) => {
         .setThumbnail(Mute.user.avatarURL)
         .setTimestamp(message.createdAt)
         .setDescription(`<@${Mute.id}> has been muted`)
-        .addField("There was no reason given", `so I dont know why <@${Mute.id}> was muted`)
+        .addField("There was no reason given", `so I don't know why <@${Mute.id}> was muted`)
 
         message.delete().catch(O_o=>{});
         mutechan.send(muteembed2);
