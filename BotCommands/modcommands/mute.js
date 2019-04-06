a95314a2c44306f97bc0a912a1502adde376d370
CloroxBleach6 <39915632+CloroxBleach6@users.noreply.github.com>
2019-04-06 14:03:54 -0400

    Update mute.js

--- BotCommands/modcommands/mute.js
+++ BotCommands/modcommands/mute.js
@@ -14,83 +14,45 @@ exports.run = async(bot, message, args) => {
     if(!Mute) return message.reply(errorEmbed);
     let arg = message.content.toLowerCase(); 
     let mreason = args.join(" ").slice(22);
-    let mreasonS = args.join(" ").slice(32);
-    if(Mute.hasPermission("MANAGE_MESSAGES")) return message.reply("Can't mute them!");   
-
-    if(arg === `;mute server ${Mute}`) {
-        message.guild.channels.forEach(channel => {
-            channel.overwritePermissions(message.guild.members.get(Mute.id),{
-                SEND_MESSAGES: false
-            });
-         });
-        
-        let mutechanS = message.guild.channels.find(b => b.name === "modlogs");
-        if(!mutechanS) return message.channel.send("Could not find modlogs channel");
-        if(!mreasonS) {
-            
-        let muteembed2S = new Discord.RichEmbed()
-        .setAuthor('Muted by' + message.author.username)
-        .addField('Muted in', message.channel.name)
-        .setColor('PURPLE')
-        .setThumbnail(Mute.user.avatarURL)
-        .setTimestamp(message.createdAt)
-        .setDescription(`<@${Mute.id}> has been muted in all channels`)
-        .addField("There was no reason given", `so I don't know why <@${Mute.id}> was muted`)
-
-        message.delete().catch(O_o=>{});
-        mutechanS.send(muteembed2S);
-
-      }else{
-        let muteembedS = new Discord.RichEmbed()
-        .setAuthor('Muted by' + message.author.username)
-        .addField('Muted in', message.channel.name)
-        .setColor('PURPLE')
-        .setThumbnail(Mute.user.avatarURL)
-        .setTimestamp(message.createdAt)
-        .setDescription(`<@${Mute.id}> has been muted in all channels`)
-        .addField("Reason for mute", mreasonS)
-
-        message.delete().catch(O_o=>{});
-        mutechanS.send(muteembedS);
-        return;
-      }
-        
-    }else{
-        message.channel.overwritePermissions(message.guild.members.get(Mute.id),{
+    
+    if(Mute.hasPermission("MANAGE_MESSAGES")) return message.reply("Can't mute them!"); 
+    
+     message.guild.channels.forEach(channel => {
+        channel.overwritePermissions(message.guild.members.get(Mute.id),{
             SEND_MESSAGES: false
         });
+     });
     
-        let mutechan = message.guild.channels.find(b => b.name === "modlogs");
-        if(!mutechan) return message.channel.send("Could not find modlogs channel");
-        if(!mreason){
+    let mutechan = message.guild.channels.find(b => b.name === "modlogs");
+    if(!mutechan) return message.channel.send("Could not find modlogs channel");
+    if(!mreason){
 
-        let muteembed2 = new Discord.RichEmbed()
-        .setAuthor('Muted by' + message.author.username)
-        .addField('Muted in', message.channel.name)
-        .setColor('PURPLE')
-        .setThumbnail(Mute.user.avatarURL)
-        .setTimestamp(message.createdAt)
-        .setDescription(`<@${Mute.id}> has been muted`)
-        .addField("There was no reason given", `so I don't know why <@${Mute.id}> was muted`)
+    let muteembed2 = new Discord.RichEmbed()
+    .setAuthor('Muted by' + message.author.username)
+    .addField('Muted in', message.channel.name)
+    .setColor('PURPLE')
+    .setThumbnail(Mute.user.avatarURL)
+    .setTimestamp(message.createdAt)
+    .setDescription(`<@${Mute.id}> has been muted`)
+    .addField("There was no reason given", `so I don't know why <@${Mute.id}> was muted`)
 
-        message.delete().catch(O_o=>{});
-        mutechan.send(muteembed2);
+    message.delete().catch(O_o=>{});
+    mutechan.send(muteembed2);
 
-      }else{
-        let muteembed = new Discord.RichEmbed()
-        .setAuthor('Muted by' + message.author.username)
-        .addField('Muted in', message.channel.name)
-        .setColor('PURPLE')
-        .setThumbnail(Mute.user.avatarURL)
-        .setTimestamp(message.createdAt)
-        .setDescription(`<@${Mute.id}> has been muted`)
-        .addField("Reason for mute", mreason)
+  }else{
+    let muteembed = new Discord.RichEmbed()
+    .setAuthor('Muted by' + message.author.username)
+    .addField('Muted in', message.channel.name)
+    .setColor('PURPLE')
+    .setThumbnail(Mute.user.avatarURL)
+    .setTimestamp(message.createdAt)
+    .setDescription(`<@${Mute.id}> has been muted`)
+    .addField("Reason for mute", mreason)
 
-        message.delete().catch(O_o=>{});
-        mutechan.send(muteembed);
-        return;
-      }
-   }
+    message.delete().catch(O_o=>{});
+    mutechan.send(muteembed);
+    return;
+  }
 }
 
 exports.config = {
