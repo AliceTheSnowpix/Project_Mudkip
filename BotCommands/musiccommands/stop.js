1cb751a1d217f9059d61c2595d547375d51e0055
CloroxBleach6 <39915632+CloroxBleach6@users.noreply.github.com>
2019-07-19 13:34:32 -0400

    Update stop.js

--- BotCommands/musiccommands/stop.js
+++ BotCommands/musiccommands/stop.js
@@ -1,22 +1,17 @@
-exports.run = async(bot, message, args, ops) => {
-    if (!message.member.voiceChannel) {
-        return message.reply("Sorry You are not connected to a voice channel");
-    }
-    else if (!message.guild.me.voiceChannel) {
-        return message.reply("Sorry I am not in a voice channel");
-    }
-    else if (message.guild.me.voiceChannelID !== message.member.voiceChannelID) {
-        return message.reply("Sorry you are not in the same voice channel as me");
-    }
-
-    message.guild.me.voiceChannel.leave();
-    message.channel.send("Leaving the voice channel");
+exports.run = async(bot, message, args) => {
+  const queue = bot.queue;
+  const serverQueue = queue.get(message.guild.id);
+  if (!message.member.voiceChannel) return message.channel.send('You are not in the same voice channle as me.');
+  if (!serverQueue) return message.channel.send('there is nothing to stop.');
+  serverQueue.songs = [];
+  serverQueue.connection.dispatcher.destroy();
+  return message.channel.send('I have cleared the queue and left the voice channel.')
 }
 
 exports.config = {
-    aliases: [  ]
+   aliases: [  ]
 };
 
 exports.help = {
-    name: 'stop'
+   name: 'stop'
 }
