a415102e3848fed7c7bf6e665c6994824730a716
CloroxBleach6 <39915632+CloroxBleach6@users.noreply.github.com>
2019-07-19 13:27:45 -0400

    Create forceskip.js

---
+++ BotCommands/musiccommands/forceskip.js
@@ -0,0 +1,15 @@
+exports.run = async(bot, message, args) => {
+	const queue = bot.queue;
+	const serverQueue = queue.get(message.guild.id);
+	if (!message.member.voiceChannel) return message.channel.send('You are not in the same voice channel as me.');
+	if (!serverQueue) return message.channel.send('There is nothing to skip');
+	await serverQueue.connection.dispatcher.destroy();
+}
+
+exports.config = {
+  aliases: []
+}
+
+exports.help = {
+  name: 'forceskip'
+}
