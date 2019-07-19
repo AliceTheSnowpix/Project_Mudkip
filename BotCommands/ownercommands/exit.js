f008c1abfa6577ba139ff2a7bdb79ea0111588b2
CloroxBleach6 <39915632+CloroxBleach6@users.noreply.github.com>
2019-07-19 12:56:03 -0400

    Update exit.js

--- BotCommands/ownercommands/exit.js
+++ BotCommands/ownercommands/exit.js
@@ -2,8 +2,11 @@ const ownerID = '293148538886553602'
 exports.run = async (bot, message) => {
     await message.delete().catch();
     if (message.author.id != ownerID) return message.channel.send("Bot Owner Only");
-    await message.channel.send('Restarting will be back soon, <@293148538886553602>!');
-    await process.exit(0).catch((e) => { console.error(e); });
+    require('dotenv').config()
+    let token = process.env.TOKEN;
+    await message.channel.send('Restarting will be back soon, <@293148538886553602>!')
+        .then(msg => bot.destroy())
+        .then(() => bot.login(token));
 };
   
 exports.config = {
